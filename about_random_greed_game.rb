4f47d549e07dec7ce65d12587dce082f499d44fd
Patty Pozon <ppozon@yahoo.com>
2014-06-05 10:26:26 -0700

    Change the selection and sequence of politicians

--- about_random_greed_game.rb
+++ about_random_greed_game.rb
@@ -2,19 +2,22 @@ class AboutGreed < Neo::Koan
   def test_random_play
 
     srand
-    p1 = Player.new("Mabini", :aggr)
-    p2 = Player.new("Reyes", :cons)
-    p3 = Player.new("Loyola", :aggr)
-    p4 = Player.new("Calero", :cons)
+    p7 = Player.new("Napoles", :aggr)
+    p6 = Player.new("Estrada", :aggr)
     p5 = Player.new("Santos", :aggr)
+    p4 = Player.new("Calero", :cons)
+    p2 = Player.new("Reyes", :cons)
+    p1 = Player.new("Mabini", :aggr)
+    p3 = Player.new("Loyola", :aggr)
 
-    @players = [ p1, p2, p3, p4, p5 ]
+    @players = [ p1, p2, p3, p4, p5, p6, p7 ]
     g = Game.new( @players )
 
     g.play
 
     puts
     puts
+    puts "This changes the output when run from the commandline."
 
     @players.each { | player |
       name = player.name
